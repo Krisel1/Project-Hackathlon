15719b6ce3b1d9ab97528e596a7005a234977737
xavimrg <javier.martin.mr@gmail.com>
2024-07-03 18:27:01 +0200

    create controller and service

--- src/main/java/Controller/DiseasesController.java
+++ src/main/java/Controller/DiseasesController.java
@@ -3,10 +3,7 @@ package Controller;
 import Models.Diseases;
 import Services.DiseasesServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -22,5 +19,9 @@ public class DiseasesController {
         return diseasesServices.getAllDiseases();
     }
 
+@PostMapping(path = "/diseases")
+    public Diseases createDiseases(@RequestBody Diseases newDiseases){
+        return diseasesServices.createDiseases(newDiseases);
+}
 
 }
--- src/main/java/Services/DiseasesServices.java
+++ src/main/java/Services/DiseasesServices.java
@@ -3,9 +3,12 @@ package Services;
 import Models.Diseases;
 import Repositories.IDiseasesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.config.Task;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Service
 public class DiseasesServices {
     @Autowired
     IDiseasesRepository iDiseasesRepository;
@@ -13,4 +16,10 @@ public class DiseasesServices {
     public ArrayList<Diseases> getAllDiseases() {
         return (ArrayList<Diseases>) iDiseasesRepository.findAll();
     }
+
+   public Diseases createDiseases (Diseases newDiseases){
+        return iDiseasesRepository.save(newDiseases);
+   }
+
+
 }
