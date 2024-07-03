a0ec2d63ac8df285a3641785c8ebd798774e1508
Krisel1 <kriselur@gmail.com>
2024-07-03 17:26:10 +0200

    Initialized project

---
+++ src/main/java/com/ProjectHackathon/CrudDiseases/CrudDiseasesApplication.java
@@ -0,0 +1,13 @@
+package com.ProjectHackathon.CrudDiseases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDiseasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDiseasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjectHackathon/CrudDiseases/CrudDiseasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectHackathon.CrudDiseases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDiseasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
